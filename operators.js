4e1e25b1cdd58e347bd211acfc2c244a1686bdfd
LubnaT <94034890+LubnaT@users.noreply.github.com>
2022-10-15 21:07:50 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,39 @@
+function add(a,b){
+    var a,b;
+    ans=a+b;
+    return ans;
+}
+console.log(add(5,20));
+
+function sub(x,y){
+    var x,y;
+    ans=x-y;
+    return ans;
+}
+console.log(sub(5,20));
+
+
+function mul(x,y){
+    var x,y;
+    ans=x*y;
+    return ans;
+}
+console.log(mul(5,20));
+
+function div(x,y){
+    var x,y;
+    ans=x/y;
+    return ans;
+}
+console.log(div(60,20));
+
+
+
+function remainder(x,y){
+    var x,y;
+    ans=x%y;
+    return ans;
+}
+console.log(remainder(50,20));
+
+console.log("-----------------------------");
