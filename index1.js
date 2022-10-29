dc2d4e00e683cff9dab03083fd1a027fe97cbca7
LubnaT <94034890+LubnaT@users.noreply.github.com>
2022-10-29 22:40:56 +0530

    Add files via upload

--- index1.js
+++ index1.js
@@ -13,7 +13,7 @@ let User = {
     changeAge : function (newAge){this.age = newAge} ,
     changePin : function (newPin){ this.pin = newPin} ,
     printDetails : function () {return { name : this.name , age :this.age ,address : this.address }},
-    creditBalance : function (x) { if(x<=0){return alert("Enter Positive number");}
+    creditBalance : function (x) { 
                                 this.currBal = this.currBal +  x;
          } ,
     debitBalance : function (x) { this.currBal = this.currBal -  x },
@@ -30,3 +30,36 @@ const Handler = (value , fn) => {
     fn(value)
 } 
 console.log(User);
+
+
+let arr =[]
+
+const creditfntn =() =>
+{
+    let x= Number(document.getElementById("creditinput").value);
+    if(x<=0){
+        return alert("enter positive no");
+    }
+    User.creditBalance(x)
+    document.getElementById("balance").innerHTML=User.currBal;
+    const creditValue=Number(credit.innerHTML)+Number(x)
+    document.getElementById("credit").innerHTML=creditValue;
+    console.log(arr.push(x));
+    console.log("Credit: "+arr);
+
+}
+
+const debitfntn =() => 
+{
+    let x= Number(document.getElementById("debitinput").value);
+    if(x<=0 || x>User.currBal){
+        return alert("Amount is Not sufficient to debit");
+    }
+    User.debitBalance(x)
+    document.getElementById("balance").innerHTML=User.currBal;
+    const debitValue = Number(debit.innerHTML)+Number(x);
+    document.getElementById("debit").innerHTML=debitValue;
+    console.log(arr.push(x));
+    console.log("Debit:  "+arr);
+}
+
